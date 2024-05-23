b59a4df2d04894c27f513724cd996f0d2119e7d0
unknown <ankithaanil2001@gmail.com>
2024-05-23 12:39:33 +0530

    Resolve merge conflicts

--- Solution.java
+++ Solution.java
@@ -1,19 +1,12 @@
-import java.util.*;
 class Solution {
-    public int[] buildArray(int[] nums) {
-        int n = nums.length;
-        int[] arr = new int[n];
-        for (int i = 0;i<n;i++)
-            arr[i] = nums[nums[i]];
-        return arr;        
-    }
-    public static void main(String[] args)
-    {
-        int[] nums={0,2,1,5,3,4};
-        int[] num=new int[nums.length];
-        Solution s=new Solution();
-        num=s.buildArray(nums);
-        System.out.println(Arrays.toString(num));
+    public int[] getConcatenation(int[] nums) {
         
+        int[] resultantArr = new int[2 * nums.length];
+        // Copy the numbers from input array to resultant array.
+        for (int i = 0; i < resultantArr.length; i++) {
+            // Use modulo (%) for cycling through the indix of input array.
+            resultantArr[i] = nums[i % nums.length];
+        }
+        return resultantArr;
     }
 }
